324dbf40eeab44d83d1e9aa38edb8f79d4cdffa0
vedatkokmen <vedatkoekmen@gmail.com>
2021-06-25 18:55:15 +0300

    new comp

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import React from "react";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import Layout from "./components/Layout";
-import ProfileCard from "./components/ProfileCard";
-import CardTwo from "./components/CardTwo";
-import Table from "./components/Table";
-import Modal from "./components/Modal";
+// import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+// import Layout from "./components/Layout";
+// import ProfileCard from "./components/ProfileCard";
+// import CardTwo from "./components/CardTwo";
+// import Table from "./components/Table";
+// import Modal from "./components/Modal";
+import Portfolio from "./components/Portfolio";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <RouteWrapper path="/one" component={ProfileCard} layout={Layout} />
-        <RouteWrapper path="/two" component={Table} layout={Layout} />
-        <RouteWrapper path="/three" component={CardTwo} layout={Layout} />
-      </Switch>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Switch>
+    //     <RouteWrapper path="/one" component={ProfileCard} layout={Layout} />
+    //     <RouteWrapper path="/two" component={Table} layout={Layout} />
+    //     <RouteWrapper path="/three" component={CardTwo} layout={Layout} />
+    //   </Switch>
+    // </BrowserRouter>
+    <Portfolio />
   );
 };
 
-function RouteWrapper({ component: Component, layout: Layout, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={(props) => (
-        <Layout {...props}>
-          <Component {...props} />
-        </Layout>
-      )}
-    />
-  );
-}
+// function RouteWrapper({ component: Component, layout: Layout, ...rest }) {
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) => (
+//         <Layout {...props}>
+//           <Component {...props} />
+//         </Layout>
+//       )}
+//     />
+//   );
+// }
 
 export default App;
